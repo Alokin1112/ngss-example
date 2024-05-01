10ab5417fb3d0d5701320b6f2d429a6b7716a402
Dawid Szczepankowski <dawid.szczepankowski@elzab.pl>
2024-05-01 09:58:09 +0200

    Cleaned code -> moved some methods to abstract Store class

--- projects/ngss/src/lib/store/store-signal.class.implementation.ts
+++ projects/ngss/src/lib/store/store-signal.class.implementation.ts
@@ -1,7 +1,5 @@
-import { computed, inject, Injector, signal, Signal } from "@angular/core";
+import { computed, signal, Signal } from "@angular/core";
 import { toObservable } from "@angular/core/rxjs-interop";
-import { ActionInterface } from "projects/ngss/src/lib/actions/actions.interface";
-import { Middleware, MiddlewareContext } from "projects/ngss/src/lib/middleware/middleware.interface";
 import { ReducerInterface } from "projects/ngss/src/lib/reducers/reducers.interface";
 import { StoreAdditionalConfig } from "projects/ngss/src/lib/store/store-additional-config.interface";
 import { Store } from "projects/ngss/src/lib/store/store.interface";
@@ -10,22 +8,13 @@ import { map, Observable } from "rxjs";
 export class StoreSignal<S> extends Store {
 
   private readonly state$: Signal<S>;
-  private readonly middlewares: Middleware<unknown>[];
-  private readonly injector = inject(Injector);
 
   constructor(
-    private reducers: ReducerInterface<unknown>[],
-    private config: StoreAdditionalConfig
+    protected override reducers: ReducerInterface<unknown>[],
+    protected override config: StoreAdditionalConfig
   ) {
-    super();
+    super(reducers, config);
     this.state$ = this.prepareState(this.reducers);
-    this.middlewares = this.config?.middlewares || [];
-  }
-
-  dispatch<T>(action: ActionInterface<T>): void {
-    this.middlewares?.length ?
-      this.handleMiddlewares(action) :
-      this.actionDispatcher(action);
   }
 
   select<T>(callback: (state: S) => T): Observable<T> {
@@ -50,43 +39,12 @@ export class StoreSignal<S> extends Store {
     return callback(state as S);
   }
 
-  reset(storeName: string | string[] = null): void {
-    if (!storeName) {
-      this.reducers.forEach(reducer => {
-        reducer?.reset();
-      });
-    }
-    if (!Array.isArray(storeName))
-      storeName = [storeName];
-
-    this.reducers.forEach(reducer => {
-      if (storeName.includes(reducer?.name))
-        reducer?.reset();
-    });
-  }
-
-  private handleMiddlewares(action: ActionInterface<unknown>): void {
-    const context: MiddlewareContext<unknown> = {
-      getState: () => this.selectSnapshot(state => state)
-    };
-    const middlewaresWithContext = this.middlewares.map(middleware => middleware(context));
-
-    const firstAction = middlewaresWithContext.reduceRight((next, middleware) => middleware(next), this.actionDispatcher);
-    firstAction(action);
-  }
-
-  private actionDispatcher = (action: ActionInterface<unknown>): void => {
-    this.reducers.forEach(reducer => {
-      reducer?.handleAction(action);
-    });
-  };
-
   private prepareState(reducers: ReducerInterface<unknown>[]): Signal<S> {
     const state: { name: string, val: Signal<unknown> }[] = reducers.map((reducer) => ({
       name: reducer?.name,
       val: reducer.getStateSignal(),
     }));
-    const stateSignal = signal(state)
+    const stateSignal = signal(state);
     return computed(() => stateSignal().reduce((acc, curr) => ({ ...acc, [curr.name]: curr.val() }), {} as S));
   }
 }
--- projects/ngss/src/lib/store/store.interface.ts
+++ projects/ngss/src/lib/store/store.interface.ts
@@ -1,12 +1,62 @@
-import { Injectable, Signal } from "@angular/core";
+import { inject, Injectable, Injector, Signal } from "@angular/core";
 import { ActionInterface } from "projects/ngss/src/lib/actions/actions.interface";
+import { Middleware, MiddlewareContext } from "projects/ngss/src/lib/middleware/middleware.interface";
+import { ReducerInterface } from "projects/ngss/src/lib/reducers/reducers.interface";
+import { StoreAdditionalConfig } from "projects/ngss/src/lib/store/store-additional-config.interface";
 import { Observable } from "rxjs";
 
 @Injectable()
 export abstract class Store {
-  abstract dispatch<T>(action: ActionInterface<T>): void;
+
+  protected readonly middlewares: Middleware<unknown>[];
+  protected readonly injector = inject(Injector);
+
+  constructor(
+    protected reducers: ReducerInterface<unknown>[],
+    protected config: StoreAdditionalConfig
+  ) {
+    this.middlewares = this.config?.middlewares || [];
+  }
+
   abstract select<T>(callback: (state: any) => T): Observable<T>;
   abstract selectSignal<T>(callback: (state: any) => T): Signal<T>;
   abstract selectSnapshot<T>(callback: (state: any) => T): T;
-  abstract reset(storeName?: string | string[]): void;
+
+  dispatch<T>(action: ActionInterface<T>): void {
+    this.middlewares?.length ?
+      this.handleMiddlewares(action) :
+      this.actionDispatcher(action);
+  }
+
+  reset(storeName: string | string[] = null): void {
+    if (!storeName) {
+      this.reducers.forEach(reducer => {
+        reducer?.reset();
+      });
+    }
+    if (!Array.isArray(storeName))
+      storeName = [storeName];
+
+    this.reducers.forEach(reducer => {
+      if (storeName.includes(reducer?.name))
+        reducer?.reset();
+    });
+  }
+
+  protected handleMiddlewares(action: ActionInterface<unknown>): void {
+    const context: MiddlewareContext<unknown> = {
+      getState: () => this.selectSnapshot<unknown>(state => state)
+    };
+    const middlewaresWithContext = this.middlewares.map(middleware => middleware(context));
+
+    const firstAction = middlewaresWithContext.reduceRight((next, middleware) => middleware(next), this.actionDispatcher);
+    firstAction(action);
+  }
+
+  protected actionDispatcher = (action: ActionInterface<unknown>): void => {
+    this.reducers.forEach(reducer => {
+      reducer?.handleAction(action);
+    });
+  };
+
 }
